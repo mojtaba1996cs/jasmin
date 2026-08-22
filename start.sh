758d2dab315f0e5d48c46848d3a74e49f1420692
mojtaba1996php <48107577+mojtaba1996cs@users.noreply.github.com>
2026-08-22 06:40:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-
-# 1. تنفيذ المهاجرة التلقائية (Migrations)
-echo "Running migrations..."
 php artisan migrate --force
-
-# 2. تحسين الأداء وتنظيف الكاش
-echo "Caching configuration..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# 3. بدء تشغيل خادم PHP (أو خادم الويب الخاص بك)
-echo "Starting PHP-FPM..."
+php artisan storage:link --force
 exec php-fpm
