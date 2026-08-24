e0e4387356e40453d7f31c75432094d6813eb910
mojtaba1996php <48107577+mojtaba1996cs@users.noreply.github.com>
2026-08-24 23:11:36 +0200

    Update environment variable for base URL

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -39,7 +39,7 @@ public function creator()
             return null;
         }
 
-        $baseUrl = rtrim(env('SUPABASE_PROJECT_URL'), '/');
+        $baseUrl = rtrim(env('SUPABASE_ENDPOINT'), '/');
         $bucket = 'documents';
 
         return "{$baseUrl}/storage/v1/object/public/{$bucket}/{$this->file_path}";
