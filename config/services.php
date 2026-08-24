d4e63102e41330b5a97077665973300a7d8946d2
mojtaba1996php <48107577+mojtaba1996cs@users.noreply.github.com>
2026-08-24 23:28:48 +0200

    Add Supabase configuration to services

--- config/services.php
+++ config/services.php
@@ -17,7 +17,9 @@ return [
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
-
+  'supabase' => [
+    'url' => env('SUPABASE_ENDPOINT'),
+],
     'resend' => [
         'key' => env('RESEND_API_KEY'),
     ],
